96856f99759ccc3c92d37bf04af95d2761749e32
HTMLDeveloper80 <131258663+HTMLDeveloper80@users.noreply.github.com>
2023-12-27 14:40:10 +0100

    Added level, gold and adding stats(stats are not saving need to fix it)

--- main.cpp
+++ main.cpp
@@ -6,23 +6,84 @@
 class MyCharacter {
 public:
     std::string name;
-    int power, agility, health, defense, luck;
-    MyCharacter(std::string& name, int power, int agility, int health, int defense, int luck) {
+    int power, agility, health, defense, luck, gold, xp, level, statsAdded;
+    MyCharacter(std::string& name, int power, int agility, int health, int defense, int luck, int gold, int xp, int level, int statsAdded) {
         this->name = name;
         this->power = power;
         this->agility = agility;
         this->health = health;
         this->defense = defense;
         this->luck = luck;
+        this->gold = gold;
+        this->xp = xp;
+        this->level = level;
+        this->statsAdded = statsAdded;
     }
     void showStats() {
         std::cout << std::endl;
         std::cout << "Name: " << this->name << std::endl;
+        std::cout << "Level " << this->level << std::endl;
+        std::cout << "XP: " << this->xp << std::endl;
+        std::cout << "Gold: " << this->gold << std::endl;
         std::cout << "Power: " << this->power << std::endl;
         std::cout << "Agility: " << this->agility << std::endl;
         std::cout << "Health: " << this->health << std::endl;
         std::cout << "Defense: " << this->defense << std::endl;
         std::cout << "Luck: " << this->luck << std::endl;
+    }
+    bool AddStats() {
+        bool again = true;
+        int input = 0;
+        int cost = 1 * statsAdded;
+        if(gold >= cost){
+            while (again) {
+                std::cout << std::endl;
+                std::cout << "Which stat you want to increase: ";
+                std::vector<std::string> options = { "Power","Agility","Health","Defense","Luck" };
+                std::cout << std::endl;
+                for (int i = 0; i < 5; i++) {
+                    std::cout << i + 1 << " -> " << options[i] << std::endl;
+                }
+                std::cout << "Your choice: ";
+                std::cin >> input;
+                again = false;
+                switch (input)
+                {
+                case 1:
+                    power += 1;
+                    gold -= cost;
+                    std::cout << "Statistics have been added" << std::endl;
+                    statsAdded++;
+                    return true;
+                    break;
+                case 2:
+                    this->agility += 1;
+                    this->gold -= cost;
+                    break;
+                case 3:
+                    this->health += 1;
+                    this->gold -= cost;
+                    break;
+                case 4:
+                    this->defense += 1;
+                    this->gold -= cost;
+                    break;
+                case 5:
+                    this->luck += 1;
+                    this->gold -= cost;
+                    break;
+                default:
+                    std::cout << "Wrong choice!" << std::endl;
+                    again = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "You dont have enough gold for it." << std::endl;
+            std::cout << "You need " << cost << " gold to add stat." << std::endl;
+        }
     }
 };
 
@@ -56,7 +117,7 @@ MyCharacter Intro() {
             }
         }
     }
-    MyCharacter person(name, stats[0], stats[1], stats[2], stats[3], stats[4]);
+    MyCharacter person(name, stats[0], stats[1], stats[2], stats[3], stats[4], 1, 0, 1, 1);
     return person;
 }
 
@@ -77,8 +138,7 @@ int Character(MyCharacter person) {
         switch (input)
         {
         case 1:
-            std::cout << "Tu bedzie dodawanie statystyk" << std::endl;
-            return false;
+            again = person.AddStats();
             break;
         case 2:
             return true;
